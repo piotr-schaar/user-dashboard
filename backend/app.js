88bfbb5bd5f244430a87f2cecb57b82ac2d91ec8
Piotr Schaar <pschaar@tidk.pl>
2019-07-30 15:42:18 +0200

    add dotenv and set env

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
+const dotenv = require("dotenv/config");
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.NODE_DATABASE, { useNewUrlParser: true });
+
 app.listen(PORT, console.log(`Server launched on port ${PORT}`));
