9641cc61042ad9a521d8d77d736f6d0d7dfd5941
Piotr Schaar <pschaar@tidk.pl>
2019-07-31 11:57:57 +0200

    set files structure

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,43 @@
 const dotenv = require("dotenv/config");
+const session = require("express-session");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
-const app = express();
+const passport = require("passport");
+const morgan = require("morgan");
+const cors = require("cors");
+const LocalStrategy = require("passport-local").Strategy;
+const routes = require("./routes");
+const User = require("./models/User");
 
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
 app.use(bodyParser.json());
+app.use(morgan("combined"));
+app.use(cors());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(
+  session({
+    secret: "moviehub",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.NODE_DATABASE, { useNewUrlParser: true });
 
-app.listen(PORT, console.log(`Server launched on port ${PORT}`));
+const conn = mongoose.connection;
+conn.on("error", console.error.bind(console, "connection error:"));
+conn.once("open", () => {
+  console.log("Connected to database!");
+  app.listen(PORT, () => console.log(`App is listening on port ${PORT}!`));
+  app.use("/api", routes);
+});
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const { user } = require("../controllers");
+
+const router = express.Router();
+
+router.post("/user/login", user.userLogin);
+router.post("/user/logout", user.userLogout);
+router.post("/user/register", user.userRegister);
+
+module.exports = router;
