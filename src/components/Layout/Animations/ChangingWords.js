18c16873d6369cd2d91bd92ab9408516db104dca
Piotr Schaar <piotr.schaar@gmail.com>
2019-10-03 14:05:28 +0200

    separate animation-wrapper to component

---
+++ src/components/Layout/Animations/ChangingWords.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import styled, { keyframes } from 'styled-components';
+
+const animate = keyframes`
+  0% {
+    opacity: 0;
+    transform:translateY(-50%);
+  }
+  2% {
+    opacity: 1;
+    transform:translateY(0);
+  }
+  18% {
+    opacity: 1;
+    transform:translateY(0px);
+  }
+  20% {
+    opacity: 0;
+    transform:translateY(50px);
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(50px)
+  }
+`;
+
+const animationChilds = (i, duration) =>
+  `
+    &:nth-child(${i + 1}) {
+      animation-delay: ${duration * 2}s;
+    }
+  `;
+const getAnimation = length => {
+  let str = '';
+  for (let index = 0; index < length; index += 1) {
+    str += animationChilds(index, index);
+  }
+  return str;
+};
+
+const ChangingWord = ({ words }) => {
+  const [length, setLength] = useState(Number);
+  const [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    setArr(words);
+  }, []);
+
+  useEffect(() => {
+    setLength(arr.length);
+  }, [words]);
+
+  const AnimationWrapper = styled.div`
+    position: relative;
+    padding: 20px 0;
+    span {
+      font-size: 19px;
+      position: absolute;
+      top: 0;
+      overflow: hidden;
+      animation: ${animate} ${length * 2}s linear infinite;
+      opacity: 0;
+      ${getAnimation(length)}
+    }
+  `;
+  return (
+    <AnimationWrapper>
+      {arr.map(item => (
+        <span key={item.id}>{item.name}</span>
+      ))}
+    </AnimationWrapper>
+  );
+};
+
+ChangingWord.propTypes = {
+  words: PropTypes.array.isRequired,
+};
+
+export default ChangingWord;
--- src/components/Sections/Main/TasksWidget/index.js
+++ src/components/Sections/Main/TasksWidget/index.js
@@ -1,83 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import styled, { keyframes } from 'styled-components';
+import React from 'react';
+import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import Heading from 'components/Layout/Heading';
-
-const animate = keyframes`
-  0% {
-    opacity: 0;
-    transform:translateY(-50%);
-  }
-  2% {
-    opacity: 1;
-    transform:translateY(0);
-  }
-  18% {
-    opacity: 1;
-    transform:translateY(0px);
-  }
-  20% {
-    opacity: 0;
-    transform:translateY(50px);
-  }
-  100% {
-    opacity: 0;
-    transform: translateY(50px)
-  }
-`;
+import ChangingWords from 'components/Layout/Animations/ChangingWords';
 
 const WidgetWrapper = styled.div`
   margin: 0 15px;
-  padding: 10px;
+  padding: 20px;
   border: 2px solid ${({ theme }) => theme.greenOpacity};
   border-radius: 20px;
   width: 100%;
 `;
-
-const animationChilds = (i, duration) => {
-  return `
-    &:nth-child(${i + 1}) {
-      animation-delay: ${(duration) * 2}s;
-    }
-  `;
-};
-
-const getAnimation = length => {
-  let str = '';
-  for (let index = 0; index < length; index += 1) {
-    str += animationChilds(index, index);
-  }
-  return str;
-};
-
-
 
 const TasksWidget = () => {
   const tasks = useSelector(({ TasksReducer }) =>
     TasksReducer.tasks.filter(item => !item.completed),
   );
 
-  const AnimationWrapper = styled.div`
-    position: relative;
-    padding: 20px;
-    span {
-      position: absolute;
-      top: 0;
-      overflow: hidden;
-      animation: ${animate} ${tasks.length *2}s linear infinite;
-      opacity: 0;
-      ${getAnimation(tasks.length)}
-    }
-  `;
-
   return (
     <WidgetWrapper>
       <Heading>What to do today?</Heading>
-      <AnimationWrapper>
-        {tasks.map(item => (
-          <span key={item.id}>{item.name}</span>
-        ))}
-      </AnimationWrapper>
+        <ChangingWords words={tasks} />
     </WidgetWrapper>
   );
 };
