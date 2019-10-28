bd14820739ea45ddd7f2c11c8dec7e8fdbc4bd77
Piotr Schaar <piotr.schaar@gmail.com>
2019-10-28 16:42:03 +0100

    fixed inputs-form in contacts

--- src/components/Sections/Contacts/Form/useInputs/useInputs.js
+++ src/components/Sections/Contacts/Form/useInputs/useInputs.js
@@ -3,18 +3,24 @@ import Input from 'components/Layout/Input';
 
 const useInputs = (values, callback) => {
   const valuesArray = Object.keys(values);
-  return valuesArray.map(value => (
-    <Input
-      key={value}
-      id={value}
-      name={value}
-      type="text"
-      required
-      placeholder={value}
-      onChange={callback}
-      onBlur={callback}
-    />
-  ));
+
+  return valuesArray.map(value => {
+    if (value === 'id' || value === 'isFavorite') {
+      return null;
+    }
+    return (
+      <Input
+        key={value}
+        id={value}
+        name={value}
+        type="text"
+        required
+        placeholder={value}
+        onChange={callback}
+        onBlur={callback}
+      />
+    );
+  });
 };
 
 export default useInputs;
